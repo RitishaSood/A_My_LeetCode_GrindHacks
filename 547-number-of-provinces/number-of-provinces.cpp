57b2067ff619df8f9b94f2d1016824edb17f7bf8
Ritisha Sood <ritisha2935@gmail.com>
2025-11-07 15:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (93.05%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,52 +1,25 @@
 class Solution {
-private:
-    void bfs( vector<vector<int>> &adj,int node, vector<int> &vis){
-      queue <int> q;
-      //vector<int > bfs;
-      vis[node]=1;
-      
-      q.push(node);
-      while(!q.empty()){
-        int ONode = q.front();
-        q.pop();
-         
-       // bfs.push_back(ONode);
-        
-        
-        for(auto it : adj[ONode]){
-            if(!vis[it]){
-              vis[it]=1;
-              q.push(it);
-
-            }
-        } 
-      }
-
-    }
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-         int V = isConnected.size();
-        vector<vector<int>> adj(V);
-        //Step 1 to convert the matrix into Adj list 
-        for(int i = 0;i<isConnected.size();i++){
-            for(int j = 0 ; j<isConnected[0].size();j++){
-             if(isConnected[i][j]==1&&i!=j){
-                adj[i].push_back(j);
-                adj[j].push_back(i);
-             }
-            }}
-        //to chcek for province 
-       
-        vector<int> vis(V,0);
-    int cnt = 0;
-    for(int i =0 ; i<V;i++){
-    if(vis[i]==0){
-       int node = i;
-       cnt ++;
-       bfs(adj,node,vis);
-       }
-    }
-    return cnt ;
+    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visited) {
+        visited[node] = true;
+        for (int j = 0; j < isConnected.size(); ++j) {
+            if (isConnected[node][j] == 1 && !visited[j]) {
+                dfs(j, isConnected, visited);
+            }
         }
-    
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int provinces = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
 };
