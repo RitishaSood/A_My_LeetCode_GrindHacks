3d271e40ebb028a31d731b2d1b5c2a799222dd82
Ritisha Sood <ritisha2935@gmail.com>
2025-11-09 03:49:21 +0530

    Time: 15 ms (32.81%) | Memory: 35.1 MB (52.17%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,45 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
-    public:
-    int f(vector<int>& weights,int cap,int days){
-        int day=1;
-        int sum=0;
-        for(int i = 0 ;i<weights.size();i++){
-        
-            if(sum+weights[i]>cap){
-                day+= 1;
-                sum=0;
-                
+public:
+    // Helper function: calculates the number of days needed 
+    // if the ship capacity is 'cap'
+    int f(vector<int>& weights, int cap, int days) {
+        int day = 1;
+        int sum = 0;
+
+        for (int i = 0; i < weights.size(); i++) {
+            if (sum + weights[i] > cap) {
+                day += 1;
+                sum = 0;
             }
-             sum+=weights[i];
+            sum += weights[i];
         }
         return day;
     }
-    public:
-    int sum(vector<int>& weights){
-        int sum =0;
-        for(int i = 0;i<weights.size();i++){
-            sum+=weights[i];
+
+    // Helper function: returns total sum of weights
+    int sum(vector<int>& weights) {
+        int sum = 0;
+        for (int i = 0; i < weights.size(); i++) {
+            sum += weights[i];
         }
         return sum;
     }
-public:
+
+    // Main function: binary search for minimum capacity
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
         int m = sum(weights);
-    int low = *max_element(weights.begin(), weights.end());
-      
-        int high =  5e4 * 500;
-        int ans = 0;
-        while(low<= high){
-        int mid = low +(high - low)/2;
-        if(f(weights,mid,days)<=days){
-            ans = mid;
-            high = mid-1;
 
-        }else{
-            low=mid+1;
+        int low = *max_element(weights.begin(), weights.end());  // at least heaviest package
+        int high = m;                                            // at most total weight
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (f(weights, mid, days) <= days) {
+                ans = mid;      // possible answer, try smaller
+                high = mid - 1;
+            } else {
+                low = mid + 1;  // need bigger capacity
+            }
         }
-        }
-        return ans ;
+        return ans;
     }
 };
