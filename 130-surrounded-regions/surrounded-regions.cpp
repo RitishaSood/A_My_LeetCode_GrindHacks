2c4cce4f7da929d49202426a1374f89534d690a2
Ritisha Sood <ritisha2935@gmail.com>
2025-12-08 19:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (44.88%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,36 +1,61 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int i, int j) {
-        int m = board.size(), n = board[0].size();
-        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O')
-            return;
-
-        board[i][j] = '#'; 
-        dfs(board, i + 1, j);
-        dfs(board, i - 1, j);
-        dfs(board, i, j + 1);
-        dfs(board, i, j - 1);
+    bool isValid(int i , int j , int m , int n){
+        if((i>=0&&i<m)&&(j>=0&&j<n)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void dfs(vector<vector<char>>& board, int i, int j,vector<vector<int>> &vis) {
+    if(isValid(i,j,board.size(),board[0].size()) && board[i][j]=='O'){
+    vis[i][j]=1;
+    int delrow[]={-1,0,1,0};
+    int delcol[]={0,1,0,-1};
+    for(int k = 0 ; k<4;k++){
+        int nrow = i + delrow[k];
+        int ncol = j + delcol[k];
+        if(isValid(nrow,ncol,board.size(),board[0].size())&&vis[nrow][ncol]!=1){
+            dfs(board,nrow,ncol,vis);
+        }
+    }}
     }
 
     void solve(vector<vector<char>>& board) {
-        int m = board.size(), n = board[0].size();
-//Mark border-connected 'O's as safe ('#')
-        for (int i = 0; i < m; ++i) {
-            if (board[i][0] == 'O') dfs(board, i, 0);
-            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
-        }
-        for (int j = 0; j < n; ++j) {
-            if (board[0][j] == 'O') dfs(board, 0, j);
-            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
-        }
-    // Capture surrounded regions ('O' → 'X'), and restore safe ones ('#' → 'O')
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'O')
-                    board[i][j] = 'X';
-                else if (board[i][j] == '#')
-                    board[i][j] = 'O';
+        int m = board.size();//rows
+        int n = board[0].size();//cols
+        vector<vector<int>> vis(m,(vector<int>(n,0)));
+
+for (int j = 0; j < n; j++) {
+    dfs(board, 0, j, vis);
+    dfs(board, m-1, j, vis);
+}
+
+
+for (int i = 0; i < m; i++) {
+    dfs(board, i, 0, vis);
+    dfs(board, i, n-1, vis);
+}
+
+       vector<vector<char>> ans;
+       for(int i = 0 ; i<m;i++){
+        for(int j = 0 ;j<n;j++){
+            //if(board[i][j]=='X'){
+             //   board[i][j]='X';
+           // }
+            if(vis[i][j]!=1){
+                board[i][j]='X';
+            }
+            if(vis[i][j]==1){
+                board[i][j]='O';
             }
         }
+       }
+       
     }
 };
+//Coding Mistake to Remember
+//Take care to pass ,n-1,m-1 not n and m
+//Take care of variables i,j,k
+//for char use ''and not"""
+//Always apply isValid Condition
