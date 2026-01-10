da488150cf30f2b8201152eb53a4d3bb48bd62c5
Ritisha Sood <ritisha2935@gmail.com>
2026-01-11 03:56:07 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (51.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currSum = nums[0], maxSum = nums[0];   
-        for (int i = 1; i < nums.size(); i++) {
-            currSum = max(nums[i], currSum + nums[i]);
-            maxSum = max(maxSum, currSum);
-        }      
-        return maxSum;
+       int n = nums.size(); //using kanade's algorithm
+    int maxsum = INT_MIN;
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum += nums[i];
+        if(sum>maxsum){
+            maxsum = sum;
+        }
+        if(sum<0){
+            sum = 0;
+        }
+    } 
+    return maxsum;
     }
 };
