34f5301e038b99e7a489a2538b8282857d591014
Ritisha Sood <ritisha2935@gmail.com>
2026-01-15 04:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (57.04%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    void dfs(vector<int>& c, int target, int start) {
+        if(target == 0) {
+            res.push_back(path);
+            return;
+        }
+        for(int i = start; i < c.size(); i++) {
+            if(c[i] > target) break;
+            path.push_back(c[i]);
+            dfs(c, target - c[i], i); 
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0);
+        return res;
+    }
+};
