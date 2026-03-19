152c90db4b802900c757cab80c104742f4d27f33
Ritisha Sood <ritisha2935@gmail.com>
2026-03-19 23:34:03 +0530

    Time: 59 ms (57.72%) | Memory: 130.4 MB (34.96%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -5,9 +5,10 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
 
-        vector<vector<int>> pref(rows, vector<int>(cols, 0));
+        vector<vector<int>> sum(rows, vector<int>(cols, 0));
+        vector<vector<int>> countX(rows, vector<int>(cols, 0));
 
-        int count = 0;
+        int ans = 0;
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
@@ -16,33 +17,28 @@ public:
                 if(grid[i][j] == 'X') val = 1;
                 else if(grid[i][j] == 'Y') val = -1;
 
-                pref[i][j] = val;
+                sum[i][j] = val;
+                countX[i][j] = (grid[i][j] == 'X');
 
-                if(i > 0) pref[i][j] += pref[i-1][j];
-                if(j > 0) pref[i][j] += pref[i][j-1];
-                if(i > 0 && j > 0) pref[i][j] -= pref[i-1][j-1];
+                if(i > 0){
+                    sum[i][j] += sum[i-1][j];
+                    countX[i][j] += countX[i-1][j];
+                }
+                if(j > 0){
+                    sum[i][j] += sum[i][j-1];
+                    countX[i][j] += countX[i][j-1];
+                }
+                if(i > 0 && j > 0){
+                    sum[i][j] -= sum[i-1][j-1];
+                    countX[i][j] -= countX[i-1][j-1];
+                }
 
-                // check equal X and Y AND at least one X
-                if(pref[i][j] == 0){
-                    // ensure at least one X exists
-                    // easiest way: check total cells not zero
-                    // OR track separately
-                    bool hasX = false;
-
-                    for(int x = 0; x <= i && !hasX; x++){
-                        for(int y = 0; y <= j; y++){
-                            if(grid[x][y] == 'X'){
-                                hasX = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if(hasX) count++;
+                if(sum[i][j] == 0 && countX[i][j] > 0){
+                    ans++;
                 }
             }
         }
 
-        return count;
+        return ans;
     }
 };
