2382d8ed95de7cb145c803db9e7b685130d754d0
Ritisha Sood <ritisha2935@gmail.com>
2026-03-21 17:01:15 +0530

    Time: 4 ms (17.77%) | Memory: 35.2 MB (5.12%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -4,11 +4,17 @@ public:
     vector<vector<int>> newgrid = grid;
     int n = grid.size();
     int m = grid[0].size();
-    for(int i = x ; i <= x+k-1;i++){
-        for(int j = y ; j < y+k ; j++){
-         newgrid[i][j] = grid[x+k-1-(i-x)][j];
+    // for(int i = x ; i <= x+k-1;i++){
+    //     for(int j = y ; j < y+k ; j++){
+    //      newgrid[i][j] = grid[x+k-1-(i-x)][j];
+    //     }
+    // }
+    for(int i = 0; i<k/2;i++){
+        for(int j = 0 ; j<k;j++){
+            swap(grid[x+i][y+j],grid[x+k-1-i][j+y]);
+
         }
     }
-    return newgrid;
+    return grid;
     }
 };
