d220f966fce6e9248966dc733e47928d2ca4acb2
Ritisha Sood <ritisha2935@gmail.com>
2026-04-01 05:50:09 +0530

    Time: 46 ms (19.35%) | Memory: 11.1 MB (79.03%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -1,61 +1,45 @@
 class Solution {
-    vector<int> calcZ(const string &s) {
-        int size = s.size();
-        vector<int> z(size, 0);
-        int l = 0, r = 0;   // z-box: [l, r]
-        for (int i = 1; i < size; i++) {
-            if (i <= r)
-                z[i] = min(z[i - l], r - i + 1);
-            while (i + z[i] < size && s[z[i]] == s[i + z[i]]) {
-                l = i;
-                r = i + z[i]++;
-            }
-        }
-        z[0] = size;
-        return z;
-    }
 public:
     string generateString(string str1, string str2) {
-        int size1 = str1.size(), size2 = str2.size();
-        int wordSize = size1 + size2 - 1;
-        string word(wordSize, '*');
-        vector<int> z = calcZ(str2);
+        int n = str1.length(), m = str2.length();
+        string res (n+m-1, 'a');
+        vector<bool> isFixed(n+m-1, false);
 
-        // process T
-        int pre = -size2;
-        for (int i = 0; i < size1; i++) {
-            if (str1[i] == 'F')
-                continue;
-            int residueLen = max(0, pre + size2 - i);
-            if (residueLen && z[size2 - residueLen] < residueLen)
-                return "";
-            for (int j = residueLen; j < size2; j++) {
-                word[i + j] = str2[j];
+        for(int i=0; i<n; i++){
+            if(str1[i] == 'T'){
+                for(int j=0; j<m; j++){
+                    int pos = i+j;
+                    if(isFixed[pos] && res[pos] != str2[j]) return "";
+                    res[pos] = str2[j];
+                    isFixed[pos] = true;
+                }
             }
-            pre = i;
         }
 
-        vector<int> lastWild(wordSize, -1);
-        pre = -1;
-        for (int i = 0; i < wordSize; i++) {
-            if (word[i] == '*') {
-                word[i] = 'a';
-                pre = i;
+        for(int i=0; i<n; i++){
+            if(str1[i] == 'F'){
+                int count = 0;
+                for(int j=m-1; j>=0; j--){
+                    int pos = i+j;
+                    if(res[pos] != str2[j]){
+                        count ++;
+                    }
+                }
+
+                if(count>0) continue;
+                bool changed = false;
+                for(int j=m-1; j>=0; j--){
+                    int pos = i+j;
+                    if(!isFixed[pos]){
+                        res[pos] = 'b';
+                        changed = true; break;
+                    }
+                }
+
+                if(!changed) return "";
             }
-            lastWild[i] = pre;
         }
 
-        // process F
-        z = calcZ(str2 + word);
-        for (int i = 0; i < size1; i++) {
-            if (str1[i] == 'T' || z[size2 + i] < size2)
-                continue;
-            int candidatePos = lastWild[i + size2 - 1];
-            if (candidatePos < i)
-                return "";
-            word[candidatePos] = 'b';
-            i = candidatePos;
-        }
-        return word;
+        return res;
     }
 };
