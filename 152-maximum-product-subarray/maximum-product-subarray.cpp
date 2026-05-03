c3815d2361592c3e4138f8abbfeebfc2331ed51d
Ritisha Sood <ritisha2935@gmail.com>
2026-05-03 15:47:41 +0530

    Time: 403 ms (5.01%) | Memory: 17.8 MB (58.79%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxprod = INT_MIN;
-        int suffprod = 1;
-        int preprod = 1;
-        int n = nums.size();
-        for(int i = 0;i<nums.size();i++){
-            if(suffprod == 0) suffprod = 1;
-            if(preprod == 0) preprod = 1;
-            suffprod *= nums[n-1-i];
-            preprod *= nums[i];
-            maxprod = max(maxprod,suffprod);
-            maxprod = max(maxprod,preprod);
-        }
+//-----------------------BRUTE-------FORCE-----------------------------------------------------------------
+          int maxprod = INT_MIN;
+          int n = nums.size();
+          for(int i = 0;i<n;i++){
+            int prod = 1;
+            for(int j = i;j<n;j++){ 
+                prod*=nums[j];
+                if(prod == 0) {
+                maxprod = max(prod,maxprod);
+                continue;
+                }
+                maxprod = max(prod,maxprod);
+            }
+            
+          }
+
+
+
+
+// -----------------------OPTIMAL-----SOLUTION--------------------------------------------------------------
+        // int maxprod = INT_MIN;
+        // int suffprod = 1;
+        // int preprod = 1;
+        // int n = nums.size();
+        // for(int i = 0;i<nums.size();i++){
+        //     if(suffprod == 0) suffprod = 1;
+        //     if(preprod == 0) preprod = 1;
+        //     suffprod *= nums[n-1-i];
+        //     preprod *= nums[i];
+        //     maxprod = max(maxprod,suffprod);
+        //     maxprod = max(maxprod,preprod);
+        // }
         return maxprod;
     }
 };
