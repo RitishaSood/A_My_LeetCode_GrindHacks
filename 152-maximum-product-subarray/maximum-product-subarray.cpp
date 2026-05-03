8fd87e62af0460e4d85051ae0687b6a166370bd2
Ritisha Sood <ritisha2935@gmail.com>
2026-05-03 15:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.07%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-    //     //BRUTE FORCE , find all the subarrays and give their maxProduct !
-    //     int maxprod = INT_MIN;
-    //     for(int i = 0 ; i<nums.size() ; i++){
-    //         int prod = 1;
-    //         for(int j = i ; j<nums.size() ; j++){
-    //         prod = prod*nums[j];
-    //         maxprod = max(prod,maxprod);
-    //         }
-    //     }
-    //     return maxprod;
-
-
-    // if know if all positive then entire array is the maxprod
-    //or if even number of negatives then the entire array
-    // if odd no. of negatives then the exclude the minimum one 
-    //what if , always the right prefic proguct of the left preficx product will give the max product
-    int preffixprod = 1;
-    int suffixprod = 1;
-    int maxprod = INT_MIN;
-    for(int i = 0 ; i<nums.size(); i++){
-        if(preffixprod == 0) preffixprod = 1;
-        if(suffixprod == 0) suffixprod = 1;
-        preffixprod = preffixprod * nums[i];
-        suffixprod = suffixprod * nums[nums.size()-1-i];
-        maxprod = max(maxprod,preffixprod);
-        maxprod = max(maxprod,suffixprod);
-    }
-    return maxprod;
+        int maxprod = INT_MIN;
+        int suffprod = 1;
+        int preprod = 1;
+        int n = nums.size();
+        for(int i = 0;i<nums.size();i++){
+            if(suffprod == 0) suffprod = 1;
+            if(preprod == 0) preprod = 1;
+            suffprod *= nums[n-1-i];
+            preprod *= nums[i];
+            maxprod = max(maxprod,suffprod);
+            maxprod = max(maxprod,preprod);
+        }
+        return maxprod;
     }
 };
