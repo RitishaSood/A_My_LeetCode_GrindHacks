5b583a04c080bd63b9ed7ffaf236f6e2764ec1a7
Ritisha Sood <ritisha2935@gmail.com>
2026-06-30 05:07:35 +0530

    Time: 67 ms (18.37%) | Memory: 111.3 MB (5.69%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,20 +1,33 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         // sort(nums.begin(),nums.end());
+//         // for(int i=0;i<nums.size()-1;i++)
+//         // {
+//         //     if(nums[i]==nums[i+1])  return true;
+//         // }
+//         // return false;
+//         unordered_set <int> st;
+//         for(int i = 0; i<nums.size(); i++){
+            
+//             if(st.find(nums[i] ) != st.end()){
+//                 return true;
+//             }
+//             st.insert(nums[i]);
+
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        // for(int i=0;i<nums.size()-1;i++)
-        // {
-        //     if(nums[i]==nums[i+1])  return true;
-        // }
-        // return false;
-        unordered_set <int> st;
-        for(int i = 0; i<nums.size(); i++){
-            
-            if(st.find(nums[i] ) != st.end()){
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if (seen[num] >= 1)
                 return true;
-            }
-            st.insert(nums[i]);
-
+            seen[num]++;
         }
         return false;
     }
