83ba8c2cb31d3acd96711d2ef0595ab1dc8044b5
Ritisha Sood <ritisha2935@gmail.com>
2026-07-01 14:12:55 +0530

    Time: 8 ms (34.19%) | Memory: 19.3 MB (54.97%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        //first form an adj list 
-       vector< vector<int>> adj(numCourses);
-        for(int i =0;i<prerequisites.size();i++){
-            
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            adj[a].push_back(b);
-            
+        int cnt_valid_courses = 0;
+        vector<vector<int>> adj(numCourses);
+        for(auto &it:prerequisites){
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);
+        }
 
-        }
-        vector<int> inDegree(numCourses,0);
-        //calculation of the inDegree
-        for(int i = 0 ; i<numCourses ; i++){
-            for(auto it : adj[i]){
-                inDegree[it]++;
+        // calculate in degress;
+        vector<int> indegree(numCourses,0);
+        for(auto &it : adj){
+            for(auto &adjnodes : it){
+               indegree[adjnodes]++;
             }
         }
-        queue <int> q;
-        for(int i = 0 ; i<numCourses;i++){
-            if(inDegree[i]==0){
+        queue<int> q;
+        for(int i =0; i<numCourses; i++){
+            if(indegree[i]==0){
                 q.push(i);
+
             }
         }
-        vector<int> Topo;
+        vector<int> vis(numCourses,0);
         while(!q.empty()){
-            int out = q.front();
+            auto out = q.front();
             q.pop();
-            Topo.push_back(out);
-            for(auto it : adj[out]){
-                inDegree[it]--;
-                if(inDegree[it]==0){
+            vis[out]=1;
+            cnt_valid_courses ++;
+            for(auto &it : adj[out]){
+                if(vis[it]==0){
+                indegree[it]--;
+                if(indegree[it]==0){
                     q.push(it);
+                }
                 }
             }
         }
-        if(Topo.size()==numCourses){
-            return true;
-        }else{
-            return false;
-        }
+        return cnt_valid_courses == numCourses;
+        
+        
     }
 };
