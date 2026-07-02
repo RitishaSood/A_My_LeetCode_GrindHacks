d2325576d34773e18ae1f628ce6b5eab58901a17
Ritisha Sood <ritisha2935@gmail.com>
2026-07-02 07:11:45 +0530

    Time: 3 ms (27.28%) | Memory: 17.1 MB (40.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,35 +1,58 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-        int fresh = 0, minutes = 0;
+    bool isValid(int r, int c, int n , int m){
+        if(r < n && r>=0 && c <m && c>=0){
+            return true;
+        }
+        return false;
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 2) q.push({i, j});
-                else if (grid[i][j] == 1) fresh++;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int totalOranges = 0;
+        int count_Rotten_after_conversion =0;
+        // bfs
+        int steps = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q; // orange , steps
+        
+        for(int i =0; i<n;i++){
+            for(int j =0; j<m; j++){
+                if(grid[i][j]==2){
+                    totalOranges++;
+                    q.push({{i,j},steps});
+                    vis[i][j]=1;
+
+                }else if(grid[i][j]==1){
+                    totalOranges++;
+                }
+
             }
         }
-
-        vector<int> dir = {0, 1, 0, -1, 0};
-
-        while (!q.empty() && fresh > 0) {
-            int size = q.size();
-            minutes++;
-            for (int i = 0; i < size; ++i) {
-                auto [x, y] = q.front(); q.pop();
-                for (int d = 0; d < 4; ++d) {
-                    int nx = x + dir[d], ny = y + dir[d + 1];
-                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        fresh--;
-                        q.push({nx, ny});
-                    }
+        
+        while(!q.empty()){
+            auto out = q.front();
+            q.pop();
+            count_Rotten_after_conversion++;
+            int row = out.first.first;
+            int col = out.first.second;
+            steps = out.second;
+            int rowadder[]={0,1,-1,0};
+            int coladder[]={1,0,0,-1};
+            for(int i =0; i<4;i++){
+                int nr = row+rowadder[i];
+                int nc = col +coladder[i];
+                if(isValid(nr,nc,n,m) && grid[nr][nc]==1 && vis[nr][nc] ==0){
+                    vis[nr][nc]=1;
+                    q.push({{nr,nc},steps+1});
                 }
             }
         }
-
-        return fresh == 0 ? minutes : -1;
+        if(count_Rotten_after_conversion != totalOranges) {
+            return -1;
+        }
+        return steps;
+        
     }
 };
