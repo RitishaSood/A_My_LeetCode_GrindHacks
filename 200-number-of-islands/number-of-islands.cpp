fbf4445234c9a56a7bec8a6a7f4b7fea2e22e8d1
Ritisha Sood <ritisha2935@gmail.com>
2026-07-02 08:51:16 +0530

    Time: 22 ms (88.97%) | Memory: 19.5 MB (35.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,44 @@
-class Solution {
+class Solution{
 public:
-    int m, n;
-    void dfs(vector<vector<char>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0')
-            return;
+     bool isValid(int r,int c,int n,int m){
+            if(r>=0 && r<n && c>= 0 && c<m){
+                return true;
+            }
+            return false;
 
-        grid[r][c] = '0'; // mark as visited
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty()) return 0;
-        m = grid.size();
-        n = grid[0].size();
-        int islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    dfs(grid, i, j);
+        }
+    int numIslands(vector<vector<char>> &grid){
+        // Lets Aplly bfs and traverse ;
+        queue<pair<int,int>> q;
+       
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<int> r = {1,0,-1,0};
+        vector<int> c = {0,1,0,-1};
+        int count=0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0; j<m;j++){
+                if(grid[i][j] == '1' && vis[i][j] != 1){
+                    count++;
+                   vis[i][j] = 1;
+                   q.push({i,j});
+                   while(!q.empty()){
+                    auto [row,col] = q.front();
+                    q.pop();
+                    for(int k = 0; k<4;k++){
+                        int nr = row + r[k];
+                        int nc = col + c[k];
+                        if(isValid(nr,nc,n,m) && grid[nr][nc] == '1' && vis[nr][nc] != 1){
+                            vis[nr][nc] = 1;
+                            q.push({nr,nc});
+                        }
+                    }
+                   }
                 }
+
             }
         }
-        return islands;
+        return count;
     }
 };
