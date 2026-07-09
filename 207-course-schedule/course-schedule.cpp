b5acd178ce00683e78986ff0830c600843dfc041
Ritisha Sood <ritisha2935@gmail.com>
2026-07-10 01:51:49 +0530

    Time: 11 ms (20.56%) | Memory: 19.2 MB (68.26%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int cnt_valid_courses = 0;
-        vector<vector<int>> adj(numCourses);
-        for(auto &it:prerequisites){
-            int u = it[1];
-            int v = it[0];
+        // make directed edges 
+        // add to adj 
+        // perform topo sort 
+        // if(result is equal to np. of nodes), yes possible 
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        for(auto &it : prerequisites){
+            int u = it[0];
+            int v = it[1];
             adj[u].push_back(v);
-        }
-
-        // calculate in degress;
-        vector<int> indegree(numCourses,0);
-        for(auto &it : adj){
-            for(auto &adjnodes : it){
-               indegree[adjnodes]++;
-            }
+            // adj[v].push_back(u);
+            indegree[v]++;
         }
         queue<int> q;
-        for(int i =0; i<numCourses; i++){
-            if(indegree[i]==0){
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
                 q.push(i);
-
             }
+
         }
-        vector<int> vis(numCourses,0);
+        int cnt =0;
         while(!q.empty()){
             auto out = q.front();
             q.pop();
-            vis[out]=1;
-            cnt_valid_courses ++;
-            for(auto &it : adj[out]){
-                if(vis[it]==0){
+            cnt++;
+            for(auto &it:adj[out]){
                 indegree[it]--;
                 if(indegree[it]==0){
                     q.push(it);
                 }
-                }
             }
+
         }
-        return cnt_valid_courses == numCourses;
-        
+        return cnt == numCourses;
         
     }
 };
