a6f6cbbb5de35226a1e2e4d398b4a6b378a32d8e
Ritisha Sood <ritisha2935@gmail.com>
2026-07-18 04:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (60.92%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        // hashing 
         vector<int> hash(101,0);
-        for(auto &it : logs){
-            int start = it[0];
-            int end = it[1];
-            for(int i=start; i<end;i++){
-                hash[i-1950]++;
+        for(auto &it: logs){
+           hash[it[0]-1950]++;
+           hash[it[1]-1950]--;
+        } 
+
+        // prefix sum and max ele 
+        int ans = 0;
+        int mx = hash[0];
+        for(int i = 1 ; i<101;i++){
+            hash[i]+=hash[i-1];
+            if(hash[i]>mx){
+                mx = hash[i];
+                ans =i;
             }
         }
-        int max_people =0;
-        int max_people_year = 0;
-        for(int i=0; i<101;i++){
-            if(hash[i] > max_people){
-                max_people=hash[i];
-                max_people_year = i+1950;
-            }
-        }
-        return max_people_year;
+        return ans +1950;
         
     }
 };
