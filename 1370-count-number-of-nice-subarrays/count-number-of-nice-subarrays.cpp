755ac34a0ce77a0901c06b39a14aad59be0114a8
Ritisha Sood <ritisha2935@gmail.com>
2026-09-06 21:40:06 +0530

    Time: 163 ms (5.09%) | Memory: 100.7 MB (6.01%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        count[0] = 1;      // prefix sum 0 occurs once initially
-
-        int prefix = 0, result = 0;
-
-        for (int x : nums) {
-            prefix += (x % 2);   // convert odd → 1, even → 0
-
-            if (count.find(prefix - k) != count.end()) {
-                result += count[prefix - k];
+        // if i represent even as 0 and odd as 1 then # of subarrays with sum k is the the answer
+        vector<int> v(nums.size(),0);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                v[i]=0;
+            }else{
+                v[i]=1;
             }
-
-            count[prefix]++;
+        } 
+        map <int,int> mpp;
+        mpp[0]=1;
+        int curr_sum =0;
+        int counter =0;
+        for(int i=0;i<nums.size();i++){
+            curr_sum += v[i];
+            int req = curr_sum-k;
+            if(mpp.find(req)!= mpp.end()){
+                counter+=mpp[req];
+            }
+            mpp[curr_sum]++;
         }
-
-        return result;
+        return counter;
     }
 };
