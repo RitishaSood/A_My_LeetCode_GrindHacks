950bb31a31df6e5beaf092b35d17271035685585
Ritisha Sood <ritisha2935@gmail.com>
2026-09-06 18:25:52 +0530

    Time: 4 ms (4.84%) | Memory: 11.5 MB (22.58%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,23 +1,52 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int> hash(101,0);
-        for(auto &it: logs){
-           hash[it[0]-1950]++;
-           hash[it[1]-1950]--;
-        } 
 
-        // prefix sum and max ele 
-        int ans = 0;
-        int mx = hash[0];
-        for(int i = 1 ; i<101;i++){
-            hash[i]+=hash[i-1];
-            if(hash[i]>mx){
-                mx = hash[i];
-                ans =i;
+        // 1993 1999
+        //          2000 2010
+        //     1       1
+        //    1950     1961
+        //         1960          1971
+        //                   1970       1981
+        //     1     2   1            1
+        // vector array 
+        // all years
+        // birthyear add 1
+        // death year sub 1
+        int n = logs.size();
+        int m = logs[0].size();
+        // find the minimum births year
+        int min_birth_year =logs[0][0];
+        int max_death_year = logs[0][1];
+        for(auto it:logs){
+            if(it[0]<min_birth_year){
+                min_birth_year = it[0];
             }
-        }
-        return ans +1950;
+            if(it[1]>max_death_year){
+                max_death_year = it[1];
+            }
+        } //O(N)
+        int len = (max_death_year - min_birth_year)+1;
+        vector<int> prefix(len,0);
+        for(int i=0;i<n;i++){
+            prefix[logs[i][0]-min_birth_year]+=1;
+            prefix[logs[i][1]-min_birth_year]+=(-1);
+        } // O(N)
+
+        vector<int> prefix_sum((len),0);
+        for(int i=1;i<len;i++){
+            prefix_sum[i] = prefix_sum[i-1]+prefix[i];
+        }//O(len)
+        int max_ppl = 0;
+        int ans = min_birth_year;
+        for(int i=1;i<len ; i++){
+             if(prefix_sum[i]>max_ppl){
+                ans = min_birth_year + i;
+                max_ppl = prefix_sum[i];
+             }
+        }//O(N)
+        return ans;
+
         
     }
 };
